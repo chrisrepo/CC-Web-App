09b3c21cdcee58ad3aee0c23661f39d7df8105e2
chrisrepo <christopher.repanich@student.csulb.edu>
2016-01-13 20:21:34 -0800

    Update Login Page
    Page now updates input to add an email field when signing up

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,14 +5,32 @@ var userListData = [];
 // DOM Ready =============================================================
 $(document).ready(function() {
     // Add User button click
-    $('#signUp').on('click', addUser);
+    $('#signUp').on('click', addSignUpInput);
+    // Sign In (also back button if signing up)
+    $('#signIn').on('click', signUp);
 });
 
 // Functions =============================================================
 
-// Add User
-function addUser(event) {
+// Add Sign Up Fields
+function addSignUpInput(event){
     event.preventDefault();
+    var inputCount = 0;
+    $('#inputDiv input').each(function(index, val) {
+        inputCount++;
+    });
+    //add email input if only user and pass are showing
+    if (inputCount == 2) {
+        $("#inputDiv").append("<input class='form-control inputField' id='email' name='email' type='text' placeholder='Email Address: example@gmail.com'/>");  
+        $("#signIn").text("Back");
+    } else if (inputCount == 3) {
+        //try to add the user
+        addUser();
+    } 
+    
+}
+// Add User
+function addUser() {
 
     // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
@@ -26,7 +44,8 @@ function addUser(event) {
         // If it is, compile all user info into one object
         var newUser = {
             'username': $('#user').val(),
-            'password': $('#pass').val()
+            'password': $('#pass').val(),
+            'email': $('#email').val()
         }
 
         // Use AJAX to post the object to our adduser service
@@ -39,10 +58,9 @@ function addUser(event) {
 
             // Check for successful (blank) response
             if (response.msg === '') {
-
+                alert('Thank you for signing up: ' + newUser['username']);
                 // Clear the form inputs
                 $('#inputDiv input').val('');
-
             }
             else {
 
@@ -58,3 +76,23 @@ function addUser(event) {
         return false;
     }
 };
+
+//sign up
+function signUp(event) {
+    event.preventDefault();
+
+    var inputCount = 0;
+    $('#inputDiv input').each(function(index, val) {
+        if($(this).val() === '') { inputCount++; }
+    });
+
+    if (inputCount > 2) {
+        //this button is acting as a back button to go to "orignal sign in template"
+        var removeCount = 0;
+        $('#inputDiv input').each(function(index, val) {
+            if($(this).val() === '') { removeCount++; }
+            if(removeCount > 2){ $(this).remove(); }
+        });
+        $("#signIn").text("Sign In");
+    }
+}
